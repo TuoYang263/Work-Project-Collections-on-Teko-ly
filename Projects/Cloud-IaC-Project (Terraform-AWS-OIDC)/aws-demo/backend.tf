b1487f182ff3b9f2298aab754802d9a776b8d4a5
TuoYang263 <80278249+TuoYang263@users.noreply.github.com>
2025-09-02 19:50:15 +0300

    Delete Projects/Cloud-IaC-Project (Terraform-AWS-OIDC) directory
    Remove repeat folders

--- Projects/Cloud-IaC-Project (Terraform-AWS-OIDC)/aws-demo/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-438336772967-eu-north-1"
-    key            = "aws-demo/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "tf-lock-eu-north-1"
-    encrypt        = true
-  }
-}
--- Projects/Cloud-IaC-Project (Terraform-AWS-OIDC)/aws-demo/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bucket_name" {
-  value = aws_s3_bucket.demo.bucket
-}
-
-output "iam_role_arn" {
-  value = aws_iam_role.demo_role.arn
-}
